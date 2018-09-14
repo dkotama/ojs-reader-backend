38d49ea33922c6f92fa8ae856a4b2bdc3a398889
Darma Kotama <darma.kotama@gmail.com>
2018-09-14 18:15:52 +0800

    remove .gitignore

---
+++ controllers/index.1.js
@@ -0,0 +1,9 @@
+const journals = require("./journals");
+const issues = require("./issues");
+const favourites = require("./favourites");
+
+module.exports = {
+  journals,
+  issues,
+  favourites,
+}
